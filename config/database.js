4502299113edec8930d26e42170395620028f44f
Cesar Cornejo <cesarcornejolopez10@gmail.com>
2023-05-18 18:36:10 -0600

    Conexion a DB en planetscale

--- config/database.js
+++ config/database.js
@@ -2,12 +2,18 @@ const mysql = require('mysql');
 const util = require('util');
 
 const pool = mysql.createPool({
-    connectionLimit : 10,
-    host: 'localhost',
-    port: '3307',
-    user : 'root',
-    password : '',
-    database : 'hotelimperial'
+    // connectionLimit : 10,
+    // host: 'localhost',
+    // port: '3307',
+    // user : 'root',
+    // password : '',
+    // database : 'hotelimperial'
+
+    host : 'aws.connect.psdb.cloud',
+    database : 'hotelimperial',
+    user : 'j9s8zmkt6fxi0ny1jbf4',
+    password : 'pscale_pw_5O6J0TXJ046AUySah5aumnEL09wZPhk13FgtMg8LKT3',
+    ssl : true
 });
 
 pool.query = util.promisify(pool.query);
